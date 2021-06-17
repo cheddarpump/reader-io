06857ab1853c773efcfc36c743707d9fbe54bac7
cheddarpump <you@example.com>
2021-06-17 14:35:42 -0700

    basic implementation and ideas surrounding home cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { StickyBarComponent } from './components/sticky-bar/sticky-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { LibraryComponent } from './components/library/library.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { NotesComponent } from './components/notes/notes.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, StickyBarComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    StickyBarComponent,
+    HomeComponent,
+    LibraryComponent,
+    WishlistComponent,
+    NotesComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
     AppRoutingModule,
   ],
   providers: [],
